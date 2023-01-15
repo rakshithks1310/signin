93de36068d9cdf684013706cc6b4ac2d94b08470
Rakshith <rakshithks1310@gmail.com>
2023-01-15 14:21:10 +0530

    profile-page

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,19 +1,19 @@
-import Container from '@mui/material/Container';
-import Tooltip from '@mui/material/Tooltip';
 import { useLocation } from 'react-router-dom';
 import Header from './header';
 import './profile.css';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis } from 'recharts';
+// import Col from 'react-bootstrap/Col';
+// import Row from 'react-bootstrap/Row';
+// import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis } from 'recharts';
+// import { color } from '@mui/system';
 
-const data = [
-  { name: 'User 1', users: 2000000 },
-  { name: 'User 2', users: 1500000 },
-  { name: 'User 3', users: 1000000 },
-  { name: 'User 4', users: 500000 },
-];
+// const data = [
+//   { name: 'User 1', users: 2000000 },
+//   { name: 'User 2', users: 1500000 },
+//   { name: 'User 3', users: 1000000 },
+//   { name: 'User 4', users: 500000 },
+// ];
 
 const chart = {
   display: 'flex',
@@ -25,9 +25,11 @@ const div_first = {
   display: 'flex',
   flexDirection: 'row',
   marginBottom: '60px',
-  marginLeft: '35px',
-  justifyContent: 'space-around',
+  justifyContent: 'space-evenly',
 };
+const div_second ={
+  display: 'flex',
+}
 
 const div_inside = {
   width: '280px',
@@ -37,13 +39,15 @@ const div_inside = {
   border: 'none',
   fontSize: '22px',
   fontFamily: 'arial',
+  backgroundColor: '#002D70',
+  color: 'white',
 };
 
-const barchart = {
-  display: 'flex',
-  alignItem: 'center',
-  margin: ' 0 0 0 40px ',
-};
+// const barchart = {
+//   display: 'flex',
+//   alignItem: 'center',
+//   margin: ' 0 0 0 40px ',
+// };
 
 function Profile(props) {
   const { state } = useLocation();
@@ -58,55 +62,63 @@ function Profile(props) {
         <Header />
       </div>
 
+      <div>
+        <div className='announce'><NotificationsActiveIcon style={{ fontSize: '16px', padding : '5px 5px 0 8px' }}/> ANNOUNCEMENTS <NotificationsActiveIcon style={{ fontSize: '16px', padding : '5px 5px 0 8px' }}/></div>
+        <div className='annouce_inside'>We are taking a down-time on 15th January 2023 from 10:00am to 11:00am to bring you an event better expirence </div>
+      </div>
+
 
       <div style={chart}>
-        <div className="split" style={{ marginTop: '70px' }}>
+        <div className="split" >
           <div style={div_first}>
             <div>
-              <p className="para">Total Members</p>
+              <div className="para1">Total Members</div><br />
 
-              <button style={div_inside}>10</button>
+              <div className='div_inside'>127,000</div>
             </div>
 
             <div>
-              <p className="para">Total Earn Transactions</p>
-              <button style={div_inside}>4</button>
+              <div className="para">  New Members <br />
+                enrolled in last 30 days</div>
+              <div className='div_inside'></div>
+            </div>
+
+            <div>
+              <div className="para">
+
+                Renewals<br />
+                in last 30 days
+              </div>
+              <div className='div_inside'></div>
+            </div>
+            <div>
+              <div className="para">
+                Membership expiry <br />
+                in last 30 days
+                days
+              </div>
+              <div className='div_inside'></div>
             </div>
           </div>
 
-          <div style={div_first}>
+          <div style={div_second}>
             <div>
-              <p className="para">
-                Members enrolled in last 30 <br />
-                days
-              </p>
-              <button style={div_inside}>25</button>
+              <div className="para">Total Redeem<br />
+                Transactions</div>
+              <div className='div_inside'></div>
             </div>
             <div>
-              <p className="para">
-                Earn Transactions in last 30 <br />
-                days
-              </p>
-              <button style={div_inside}>89</button>
-            </div>
-          </div>
+              <div className="para">
+                Redeem Transactions <br />
+                in last 30 days
 
-          <div style={div_first}>
-            <div>
-              <p className="para">Total Redeem Transactions</p> <br />
-              <button style={div_inside}>78</button>
-            </div>
-            <div>
-              <p className="para">
-                Redeem Transactions in last 30
-                <br /> days
-              </p>
-              <button style={div_inside}>15</button>
+              </div>
+              <div className='div_inside'></div>
             </div>
           </div>
         </div>
 
-        <div style={barchart}>
+        {/* <div style={barchart}>
           <Container>
             <Row>
               <Col>
@@ -141,7 +153,7 @@ function Profile(props) {
               </Col>
             </Row>
           </Container>
-        </div>
+        </div> */}
       </div>
     </div >
   );
